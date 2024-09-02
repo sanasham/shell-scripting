f22f2f211d14af2da1bba7f716be83db9116af20
sanasham <sayyedesubvali@gmail.com>
2024-09-02 12:06:12 +0530

    basic command of vi editor

---
+++ abhishek-veeramalla/first-shell-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+with the help of O letter i have inserted new line
+this is new change first-shell-script.sh
+this is sample file 
+and this will give more flexible to user and 
+this is just learning purpose test file 1
+this is moving into lines 
+could you please share with your friends 
+this is empty line 10
+this is test file and user want to open this file just use vi editor to edit this file
+test
